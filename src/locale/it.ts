e513faf58439c8214822f78054a46820eddf096d
loremedda <lorenzo.medda.sa@nttdata.com>
2022-03-18 16:37:23 +0100

    preparation for assistance localization

---
+++ src/locale/it.ts
@@ -0,0 +1,15 @@
+export default {
+  loginPage: {
+    title: 'Accedi con SPID o CIE',
+    description:
+      'Seleziona la modalità di autenticazione che preferisci e inizia il processo di adesione',
+    loginBox: {
+      title: 'Login',
+      spidLogin: 'Entra con SPID',
+      cieLogin: 'Entra con CIE',
+    },
+    hintText: 'Non hai SPID?<1> Scopri di più</1>',
+    privacyAndCondition:
+      'Autenticandoti dichiari di aver letto e compreso l< & > <1>Informativa Privacy</1> e i <1>Termini e condizioni d’uso</1> del Portale Self Care',
+  },
+};
