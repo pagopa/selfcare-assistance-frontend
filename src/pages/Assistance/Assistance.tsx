68082fa3fe02c1e6c14e4eb0deb0547c16f3c688
USERSAD\esignore <elena.signore@nttdata.com>
2022-04-13 10:36:40 +0200

    white_theme

--- src/pages/Assistance/Assistance.tsx
+++ src/pages/Assistance/Assistance.tsx
@@ -15,6 +15,7 @@ import {
 import { uniqueId } from 'lodash';
 import { trackEvent } from '@pagopa/selfcare-common-frontend/services/analyticsService';
 import { useTranslation } from 'react-i18next';
+import withLogin from '@pagopa/selfcare-common-frontend/decorators/withLogin';
 import { useAppSelector } from '../../redux/hooks';
 import { saveAssistance } from '../../services/assistanceService';
 import { LOADING_TASK_SAVE_ASSISTANCE } from '../../utils/constants';
@@ -323,4 +324,4 @@ const Assistance = () => {
   );
 };
 
-export default Assistance;
+export default withLogin(Assistance);
