ac35389e428704ed13ee0b5670640be67b7ae2bf
loremedda <98110186+loremedda@users.noreply.github.com>
2024-04-02 12:41:31 +0200

    [SELC-4522] feat: Built the form with mode hidden directly on the page
    * temp commit for cors error
    
    * add target blank
    
    * feat: Hidden form that make submit when response is retrieved
    
    * temp commit
    
    * removed unused option
    
    ---------
    
    Co-authored-by: OraldoDoci <oraldo.doci@nttdata.com>

--- src/services/assistanceService.ts
+++ src/services/assistanceService.ts
@@ -3,7 +3,11 @@ import { DashboardApi } from '../api/DashboardApiClient';
 export const sendRequestToSupport = async (email: string, productId: string): Promise<string> => {
   /* istanbul ignore if */
   if (process.env.REACT_APP_API_MOCK_ASSISTANCE === 'true') {
-    return new Promise((resolve) => resolve('mockedUrl'));
+    return new Promise((resolve) =>
+      resolve(
+        '<html><head></head><body><form id="jwtForm" method="POST" action="www.mockedurl.com"><input id="jwtString" type="hidden" name="jwt" value="dummyToken"/><input id="returnTo" type="hidden" name="return_to" value="dummyUrl"/></form><script>window.onload = () => { document.forms["jwtForm"].submit(); };</script></body></html>'
+      )
+    );
   } else {
     return DashboardApi.sendSupportRequest(email, productId);
   }
