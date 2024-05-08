04df7cd03c6c9953575838ce88f5e1bc03924228
loremedda <lorenzo.medda@nttdata.com>
2024-05-08 15:28:41 +0200

    [SELC-4681] feat: Client-side re-adaptation following changes on the API call for access to zendesk + consequent clean code

--- src/model/ZendeskAuthorizationDTO.ts
+++
@@ -1,5 +0,0 @@
-export type ZendeskAuthorizationDTO = {
-  action_url: string;
-  return_to: string;
-  jwt: string;
-};
--- src/services/assistanceService.ts
+++ src/services/assistanceService.ts
@@ -1,12 +1,14 @@
 import { DashboardApi } from '../api/DashboardApiClient';
+import { SupportResponse } from '../api/generated/b4f-dashboard/SupportResponse';
 
-export const sendRequestToSupport = async (email: string, productId: string): Promise<string> => {
+export const sendRequestToSupport = async (
+  email: string,
+  productId: string
+): Promise<SupportResponse> => {
   /* istanbul ignore if */
   if (process.env.REACT_APP_API_MOCK_ASSISTANCE === 'true') {
     return new Promise((resolve) =>
-      resolve(
-        '<html><head></head><body><form id="jwtForm" method="POST" action="www.mockedurl.com"><input id="jwtString" type="hidden" name="jwt" value="dummyToken"/><input id="returnTo" type="hidden" name="return_to" value="dummyUrl"/></form><script>window.onload = () => { document.forms["jwtForm"].submit(); };</script></body></html>'
-      )
+      resolve({ jwt: 'mockJwt', actionUrl: 'mockActionUrl', redirectUrl: 'mockRedirectUrl' })
     );
   } else {
     return DashboardApi.sendSupportRequest(email, productId);
