3a69f1efbb824b1b6a4cb86299863498f09091da
logos-trad <155662221+logos-trad@users.noreply.github.com>
2024-11-14 12:52:23 +0100

    Consegna traduzioni selfcare-assistance-frontend [SEND]
    Consegna traduzioni selfcare-assistance-frontend [SEND]

---
+++ src/locale/en.ts
@@ -0,0 +1,31 @@
+export default {
+  session: {
+    expired: {
+      title: 'Session expired',
+      message: 'You are being redirected to the login page...',
+    },
+  },
+  assistancePage: {
+    unloadEvent: {
+      title: 'Do you really want to exit?',
+      description: 'If you exit, your request for support will be lost.',
+    },
+    dataValidate: {
+      invalidEmail: 'The email address is not valid',
+      notEqualConfirmEmail:
+        "The confirmation email address is not the same as the email address that was entered",
+    },
+    title: 'Do you need help for the Reserved Area for institutions?',
+    subTitle:
+      "Enter the email address where you want to receive the support answers. You can report problems related to registering or using the Reserved Area for your institution.",
+    email: {
+      label: 'Enter your email address',
+    },
+    confirmEmail: {
+      label: 'Confirm your email address',
+    },
+    back: 'Go back',
+    forward: 'Go forward',
+    linkPrivacyPolicy: 'By continuing, you declare to have read the <1>Support Privacy Policy</1>',
+  },
+};
