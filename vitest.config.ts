3c5142c83dbced667ec82a573029ff1443612574
GabrieleMaiocchiFilippo <fmaiocch@emeal.nttdata.com>
2026-01-20 16:56:35 +0100

    update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -9,6 +10,12 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
     include: ['src/**/*.{test,spec}.{ts,tsx}'],
+    alias: {
+      '@pagopa/selfcare-common-frontend/lib/decorators/withLogin': path.resolve(
+        __dirname,
+        'src/__mocks__/@pagopa/selfcare-common-frontend/decorators/withLogin.tsx'
+      ),
+    },
     coverage: {
       provider: 'v8',
       exclude: ['src/index.tsx', 'src/reportWebVitals.ts', 'src/api/generated/**'],
