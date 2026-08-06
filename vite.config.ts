cd2a790ebeef7d7dd5f1a17c94437d2c7d16e65c
GabrieleMaiocchiFilippo <fmaiocch@emeal.nttdata.com>
2026-08-06 18:24:29 +0200

    Fix: update vite version and all of his related

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
-/// <reference types="vitest" />
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig(({ mode }) => {
@@ -10,7 +8,6 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       react(),
-      tsconfigPaths(),
       createHtmlPlugin({
         inject: {
           data: {
@@ -32,12 +29,6 @@ export default defineConfig(({ mode }) => {
     },
     define: {
       'process.env': {},
-    },
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts',
-      include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     },
   };
 });
